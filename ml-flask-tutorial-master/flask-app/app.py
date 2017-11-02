8620a0571a4915dfd79da455b6488903f10d0cb8
Asmattic <athsmatic@gmail.com>
2017-11-02 17:26:35 -0400

    added editor config file

--- ml-flask-tutorial-master/flask-app/app.py
+++ ml-flask-tutorial-master/flask-app/app.py
@@ -1,19 +1,24 @@
 from flask import Flask, request, render_template
-import pickle
-import numpy as np
+import pickle  # Pickle
+import numpy as np  # scientific
 
 app = Flask(__name__)
 
+"""App's home route
 
+    This will only render home page
+    TODO:   11/02/2017 Generalize a template where home can
+        just be a portion
+"""
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/getdelay',methods=['POST','GET'])
+@app.route('/getdelay',methods=['POST', 'GET'])
 def get_delay():
-    if request.method=='POST':
-        result=request.form
+    if request.method == 'POST':
+        result = request.form
         origin = result['origin']
         dest = result['dest']
         unique_carrier = result['unique_carrier']
@@ -23,7 +28,7 @@ def get_delay():
         pkl_file = open('cat', 'rb')
         index_dict = pickle.load(pkl_file)
         cat_vector = np.zeros(len(index_dict))
-        
+
         try:
             cat_vector[index_dict['DAY_OF_WEEK_'+str(day_of_week)]] = 1
         except:
@@ -44,14 +49,14 @@ def get_delay():
             cat_vector[index_dict['DEP_HOUR_'+str(dep_hour)]] = 1
         except:
             pass
-        
+
         pkl_file = open('logmodel.pkl', 'rb')
         logmodel = pickle.load(pkl_file)
         prediction = logmodel.predict(cat_vector)
-        
+
         return render_template('result.html',prediction=prediction)
 
-    
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
